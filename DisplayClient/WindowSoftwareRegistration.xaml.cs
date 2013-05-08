e8095f8f47ee8ae7faa10e629eb2e9cf1ec9d1a8
Leon Potgieter <leon@itactixsoftware.com>
2013-05-08 16:02:31 +0200

    Init
    FTP Version, some minor bugfixes, video looping at 1m intervals

---
+++ DisplayClient/WindowSoftwareRegistration.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace DisplayClient
+{
+	/// <summary>
+	/// Interaction logic for WindowSoftwareRegistration.xaml
+	/// </summary>
+	public partial class WindowSoftwareRegistration : Window
+	{
+		[DllImport("HardwareID.dll")]
+        public static extern String GetHardwareID(bool HDD,
+                                                  bool NIC,
+                                                  bool CPU,
+                                                  bool BIOS,
+                                                  string sLicenseCode);
+		
+		string hardwareID;
+        public static string RegCode;
+		
+		public WindowSoftwareRegistration()
+		{
+			this.InitializeComponent();
+            //RegCode = "Unregistered";
+            hardwareID = GetHardwareID(true, true,true, true, "R5LR-S4TQ");
+            tbHardwareID.Text = hardwareID;
+            tbInstallationCode.Focus();
+            //ABE9-6CDE-E3A2-DFB2
+			// Insert code required on object creation below this point.
+		}
+
+        private  static string  GetMD5(string text)
+        {
+           UnicodeEncoding UE = new UnicodeEncoding();
+           byte[] hashValue;
+           byte[] message = UE.GetBytes(text);
+
+           MD5 hashString = new MD5CryptoServiceProvider();
+           string hex = "";
+ 
+           hashValue = hashString.ComputeHash(message);
+           foreach (byte x in hashValue)
+            {
+                hex += String.Format("{0:x2}", x);
+            }
+            return hex;
+        }
+
+		private void btnRegister_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+            try
+            {
+               
+                string hashedHardwareID = GetMD5(hardwareID);
+                hashedHardwareID = hashedHardwareID.Substring(1, 10);
+                //MessageBox.Show(hashedHardwareID);
+
+                if (tbInstallationCode.Text == hashedHardwareID)
+                {
+                    RegCode = tbInstallationCode.Text;
+                    Properties.Settings.Default.Registration = RegCode;
+                    Properties.Settings.Default.Save();
+                    this.DialogResult = true;
+                }
+                else
+                {
+                    RegCode = "U";
+                    Properties.Settings.Default.Registration = RegCode;
+                    Properties.Settings.Default.Save();
+                    this.DialogResult = false;
+                };
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+		}
+	}
+}
